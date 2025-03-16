4ea5029ef32419ac09e1d10f1901be0d969f1f2c
muskansingh65553 <muskansingh65553@gmail.com>
2025-03-16 15:32:17 +0530

    vercel anayltics update

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SpendingProvider } from "@/contexts/SpendingContext";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -27,6 +28,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <Analytics />
       </TooltipProvider>
     </SpendingProvider>
   </QueryClientProvider>
